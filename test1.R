cf86731833682451aa8fcb222d52757a51e9212c
HelenaTV <helenavermeulen@kpnmail.nl>
2025-09-12 15:13:21 +0200

    last one maybe

--- test1.R
+++ test1.R
@@ -1,7 +1,7 @@
 save_player <- TRUE
 np <- 100
 ni <- 10
-N <- 10000
+n <- 10000
 th_r <- rnorm(np, 0, 2)
 be_r <- runif(ni, -2, 2)
 th_e <- numeric(np)
@@ -12,7 +12,7 @@ for (i in 1:N) {
   ii <- sample(1:ni, 1)
   p <- sample(1:np, 1)
   s <- sample(0:1, 1, prob = c(1 - plogis(th_r[p] - be_r[ii]),
-  plogis(th_r[p] - be_r[ii])))
+                               plogis(th_r[p] - be_r[ii])))
   th_e[p] <- th_e[p] + k * (s - plogis(th_e[p] - be_e[ii]))
   be_e[ii] <- be_e[ii] - k * (s - plogis(th_e[p] - be_e[ii]))
   if (save_player) if (p == 1) th_e_save <- c(th_e_save, th_e[p])
@@ -21,4 +21,3 @@ plot(th_r, th_e)
 plot(th_e_save)
 
 lintr::lint("test1.R")
-
