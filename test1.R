23b8ee47f7725f8600d3b684b0d56f37b951c634
mmartinhidalgo <mfhidalgo18@hotmail.com>
2025-09-12 15:43:34 +0200

    correction miguel

--- test1.R
+++ test1.R
@@ -1,18 +1,24 @@
 save_player <- TRUE
 np <- 100
-ni = 10
-N<-10000
-th_r <- rnorm(np, 0, 2); be_r <- runif(ni, -2, 2) 
-th_e <- numeric(np); be_e <- numeric(ni)
+ni <- 10
+n <- 10000
+th_r <- rnorm(np, 0, 2)
+be_r <- runif(ni, -2, 2)
+th_e <- numeric(np)
+be_e <- numeric(ni)
 th_e_save <- numeric()
 k <- .1
-for(i in 1:N) { ii <- sample(1:ni, 1); p <- sample(1:np, 1)
-  s <- sample(0:1, 1, prob = c(1 - plogis(th_r[p] - be_r[ii]), plogis(th_r[p] - be_r[ii])))
+for (i in 1:N) {
+  ii <- sample(1:ni, 1)
+  p <- sample(1:np, 1)
+  s <- sample(0:1, 1, prob = c(1 - plogis(th_r[p] - be_r[ii]),
+                               plogis(th_r[p] - be_r[ii])))
   th_e[p] <- th_e[p] + k * (s - plogis(th_e[p] - be_e[ii]))
   be_e[ii] <- be_e[ii] - k * (s - plogis(th_e[p] - be_e[ii]))
-  if(save_player) if(p == 1) th_e_save <- c(th_e_save, th_e[p])
+  if (save_player) if (p == 1) th_e_save <- c(th_e_save, th_e[p])
 }
 plot(th_r, th_e)
 plot(th_e_save)
+x <- 1
 
 lintr::lint("test1.R")
