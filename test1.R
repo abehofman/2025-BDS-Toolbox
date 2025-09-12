b24d39e34db3a96defaa0a3cc79e9da9fcb75ed0
HelenaTV <helenavermeulen@kpnmail.nl>
2025-09-12 15:09:40 +0200

    idk anymore

--- test1.R
+++ test1.R
@@ -8,10 +8,11 @@ th_e <- numeric(np)
 be_e <- numeric(ni)
 th_e_save <- numeric()
 k <- .1
-for (i in 1:N) { 
+for (i in 1:N) {
   ii <- sample(1:ni, 1)
   p <- sample(1:np, 1)
-  s <- sample(0:1, 1, prob = c(1 - plogis(th_r[p] - be_r[ii]), plogis(th_r[p] - be_r[ii])))
+  s <- sample(0:1, 1, prob = c(1 - plogis(th_r[p] - be_r[ii]),
+  plogis(th_r[p] - be_r[ii])))
   th_e[p] <- th_e[p] + k * (s - plogis(th_e[p] - be_e[ii]))
   be_e[ii] <- be_e[ii] - k * (s - plogis(th_e[p] - be_e[ii]))
   if (save_player) if (p == 1) th_e_save <- c(th_e_save, th_e[p])
